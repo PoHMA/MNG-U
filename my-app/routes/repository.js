d349a4f3ebfab187d6554745641715be777d6b46
PoHMA <miguel.poma@ceis.cs.umss.edu.bo>
2019-02-28 09:07:06 -0400

    Task-b

--- my-app/routes/repository.js
+++ my-app/routes/repository.js
@@ -9,18 +9,20 @@ var ghuser;
 
 router.get('/user',function(req,res) {
     var parametros = [];
-    var name_user;
     if(req.url.indexOf("?")){
-        var url_data = req.url.split("?");
-        var parametros = url_data[1].split("&");
-        var valores = parametros[0].split("=");
-        name_user = valores[1];
-        ghuser = client.user(name_user);;
+        ghuser = client.user(getNameUser(req));;
     }
     ghuser.repos(1, 10, function (err,body,status) {
         res.render('list-repositories.jade',{projects:body});
     });
 });
+
+function getNameUser(req) {
+    var url_data = req.url.split("?");
+    var parametros = url_data[1].split("&");
+    var valores = parametros[0].split("=");
+    return valores[1];
+}
 
 
 module.exports = router;

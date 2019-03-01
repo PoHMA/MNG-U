ca98bb42e788f5c9d9ec9d01635ab075644d778b
PoHMA <miguel.poma@ceis.cs.umss.edu.bo>
2019-02-28 22:59:13 -0400

    Task-d y paginacion

--- my-app/routes/index.js
+++ my-app/routes/index.js
@@ -6,11 +6,9 @@ var client = github.client();
 var paginacion = 0;
 
 router.get('/',function(req,res) {
-  client.get('/users', paginacion,20, function (err, status, body, headers) {    
+  client.get('/users', function (err, status, body, headers) {    
     res.render('index.jade', {users: body});
     });
-  paginacion = paginacion + 20;
 });
-
 
 module.exports = router;
--- my-app/routes/repository.js
+++ my-app/routes/repository.js
@@ -6,15 +6,46 @@ var router = express();
 var client = github.client();
 
 var ghuser;
+var number_page = 1;
+var limit_sup;
+var amount_card_page = 20;
 
 router.get('/user',function(req,res) {
     var parametros = [];
+    var number_repos;
     if(req.url.indexOf("?")){
         ghuser = client.user(getNameUser(req));;
     }
-    ghuser.repos(1, 10, function (err,body,status) {
-        console.log(body);
-        res.render('list-repositories.jade',{projects:body});
+    ghuser.info(function (err,body,status) {
+        number_repos = body.public_repos;
+        limit_sup = calculateNumberPages(number_repos);
+    });
+    ghuser.repos(1, amount_card_page, function (err,body,status) {
+        res.render('list-repositories.jade',{projects:body, User:ghuser, page:number_page});
+    });
+});
+
+router.get('/next',function(req,res) {
+    var parametros = [];
+    if (number_page < limit_sup) 
+        number_page = number_page + 1;
+    if(req.url.indexOf("?")){
+        ghuser = client.user(getNameUser(req));;
+    }
+    ghuser.repos(number_page, amount_card_page, function (err,body,status) {
+        res.render('list-repositories.jade',{projects:body, User:ghuser, page:number_page});
+    });
+});
+
+router.get('/prev',function(req,res) {
+    var parametros = [];
+    if (number_page > 1) 
+        number_page = number_page - 1;
+    if(req.url.indexOf("?")){
+        ghuser = client.user(getNameUser(req));;
+    }
+    ghuser.repos(number_page, amount_card_page, function (err,body,status) {
+        res.render('list-repositories.jade',{projects:body, User:ghuser, page:number_page});
     });
 });
 
@@ -25,5 +56,11 @@ function getNameUser(req) {
     return valores[1];
 }
 
+function calculateNumberPages(amount_pages) {
+    var answer = Math.floor(amount_pages/amount_card_page);
+    if(amount_pages%amount_card_page != 0)
+        answer = answer + 1;
+    return answer;
+}
 
 module.exports = router;
